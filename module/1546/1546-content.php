0d164e8f74309bbcb01238413b268b984b90aafe
vanthanh <thanhssntc2@gmail.com>
2020-06-24 22:29:07 +0700

    sua 1546

--- module/1546/1546-content.php
+++ module/1546/1546-content.php
@@ -1,11 +1,11 @@
 <div class="type-1546">
-    <div class="container">
+    <div class="container container-browse">
         <div class="row">
-            <div class="col-xl-6 col-md-12">
+            <div class="col-lg-6 col-md-12 topic">
                 <h4 class="header-title-medium">Hot topics. Awesome instructors.</h4>
                 <h3 class="header-title-default">Check out our trending online courses</h3>
             </div>
-            <div class="col-xl-6 browse">
+            <div class="col-lg-6 browse">
                 <a href="" class="header-browse">
                     <span class="browse-icon"><i class="fa fa-graduation-cap" aria-hidden="true"></i></span>
                     <span class="browse-text">Browse all online course</span>
@@ -15,86 +15,110 @@
     </div>
 
     <!-- swiper -->
-    
+
     <div class="swiper-container">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <a href=""">
-                        <div class="overlay-slide-link">
-                            <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
-                        </div>
-                        <img src="images/ss1.jpg" alt="ss1" class="slide-img">
-                    </a>
-                    <div class="slide-content">
-                        <a href="" class="content-link"><h3 class="content-title">Start your lorem ipsum dolor</h3></a>
-                        <a href="" class="content-link"><h5 class="content-job">Business</h5></a>
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <a href="">
+                    <img src="images/ss1.jpg" alt="ss1" class="slide-img">
+                    <div class="overlay-slide-link">
+                        <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
                     </div>
-                </div> 
-                <div class="swiper-slide">
-                    <a href=""">
-                        <div class="overlay-slide-link">
-                            <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
-                        </div>
-                        <img src="images/ss2.jpg" alt="ss2" class="slide-img">
+                </a>
+                <div class="slide-content">
+                    <a href="" class="content-link">
+                        <h3 class="content-title">Start your lorem ipsum dolor</h3>
                     </a>
-                    <div class="slide-content">
-                        <a href="" class="content-link"><h3 class="content-title">Start your lorem ipsum dolor</h3></a>
-                        <a href="" class="content-link"><h5 class="content-job">Business</h5></a>
-                    </div>
-                </div> 
-                <div class="swiper-slide">
-                    <a href=""">
-                        <div class="overlay-slide-link">
-                            <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
-                        </div>
-                        <img src="images/ss3.jpg" alt="ss3" class="slide-img">
+                    <a href="" class="content-link">
+                        <h5 class="content-job">Business</h5>
                     </a>
-                    <div class="slide-content">
-                        <a href="" class="content-link"><h3 class="content-title">Start your lorem ipsum dolor</h3></a>
-                        <a href="" class="content-link"><h5 class="content-job">Business</h5></a>
-                    </div>
-                </div> 
-                <div class="swiper-slide">
-                    <a href=""">
-                        <div class="overlay-slide-link">
-                            <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
-                        </div>
-                        <img src="images/ss4.jpg" alt="ss4" class="slide-img">
-                    </a>
-                    <div class="slide-content">
-                        <a href="" class="content-link"><h3 class="content-title">Scelerisque faucibus felis leo pharetra</h3></a>
-                        <a href="" class="content-link"><h5 class="content-job">Business</h5></a>
-                    </div>
-                </div> 
-                <div class="swiper-slide">
-                    <a href=""">
-                        <div class="overlay-slide-link">
-                            <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
-                        </div>
-                        <img src="images/ss5.jpg" alt="ss5" class="slide-img">
-                    </a>
-                    <div class="slide-content">
-                        <a href="" class="content-link"><h3 class="content-title">Start your lorem ipsum dolor</h3></a>
-                        <a href="" class="content-link"><h5 class="content-job">Business</h5></a>
-                    </div>
-                </div> 
-                <div class="swiper-slide">
-                    <a href=""">
-                        <div class="overlay-slide-link">
-                            <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
-                        </div>
-                        <img src="images/ss6.jpg" alt="ss6" class="slide-img">
-                    </a>
-                    <div class="slide-content">
-                        <a href="" class="content-link"><h3 class="content-title">Start your lorem ipsum dolor</h3></a>
-                        <a href="" class="content-link"><h5 class="content-job">Business</h5></a>
-                    </div>
-                </div> 
+                </div>
             </div>
-            <!-- Add Pagination -->
-            <div class="swiper-pagination"></div>
-            <!-- Add Arrows -->
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
+            <div class="swiper-slide">
+                <a href="">
+                    <img src="images/ss2.jpg" alt="ss2" class="slide-img">
+                    <div class="overlay-slide-link">
+                        <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
+                    </div>
+                </a>
+                <div class="slide-content">
+                    <a href="" class="content-link">
+                        <h3 class="content-title">Business ipsum for amet glavrida</h3>
+                    </a>
+                    <a href="" class="content-link">
+                        <h5 class="content-job">Business</h5>
+                    </a>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <a href="">
+                    <img src="images/ss3.jpg" alt="ss3" class="slide-img">
+                    <div class="overlay-slide-link">
+                        <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
+                    </div>
+                </a>
+                <div class="slide-content">
+                    <a href="" class="content-link">
+                        <h3 class="content-title">10 easy steps to lorem dolor glavrida</h3>
+                    </a>
+                    <a href="" class="content-link">
+                        <h5 class="content-job">Lifestyle</h5>
+                    </a>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <a href="">
+                    <img src="images/ss4.jpg" alt="ss4" class="slide-img">
+                    <div class="overlay-slide-link">
+                        <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
+                    </div>
+                </a>
+                <div class="slide-content">
+                    <a href="" class="content-link">
+                        <h3 class="content-title">How to phasellus vitae convallis</h3>
+                    </a>
+                    <a href="" class="content-link">
+                        <h5 class="content-job">Ecommerce</h5>
+                    </a>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <a href="">
+                    <img src="images/ss5.jpg" alt="ss5" class="slide-img">
+                    <div class="overlay-slide-link">
+                        <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
+                    </div>
+                </a>
+                <div class="slide-content">
+                    <a href="" class="content-link">
+                        <h3 class="content-title">Faucibus felis leo pharetra lorem</h3>
+                    </a>
+                    <a href="" class="content-link">
+                        <h5 class="content-job">Marketing</h5>
+                    </a>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <a href="">
+                    <img src="images/ss6.jpg" alt="ss6" class="slide-img">
+                    <div class="overlay-slide-link">
+                        <i class="fa fa-info-circle slide-icon" aria-hidden="true"></i>
+                    </div>
+                </a>
+                <div class="slide-content">
+                    <a href="" class="content-link">
+                        <h3 class="content-title">Scelerisque faucibus felis leo pharetra</h3>
+                    </a>
+                    <a href="" class="content-link">
+                        <h5 class="content-job">Ecommerce</h5>
+                    </a>
+                </div>
+            </div>
         </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
     </div>
+</div>
--- module/1546/js/1546.js
+++ module/1546/js/1546.js
@@ -1,37 +1,31 @@
 $(document).ready(function(){
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    });
-$(window).resize(function(){
-    var width = $(window).width();
-    if (width < 1000){
-            var swiper = new Swiper('.swiper-container', {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            slidesPerGroup: 1,
-            loop: true,
-            loopFillGroupWithBlank: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20,
             },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
             },
-            });
-        }
-    });
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }
+      });
 });
